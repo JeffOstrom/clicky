b57c7e3011b8fc0466bb093a236ea87a0141c110
Jeff Ostrom <j.ostrm@gmail.com>
2018-02-07 20:06:07 -0600

    Updates

--- src/components/nav/nav.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./nav.css";
-
-const Nav = props => (
-    <nav className="navbar"> My total score update {props.name} SCORE: {props.score}</nav>
-);
-
-export default Nav; 
